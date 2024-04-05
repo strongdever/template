e9d2d01d204d4979cf57ba8b0ef8089639184109
Strong Man <kstronger77@gmail.com>
2024-04-05 12:11:51 +0900

    202404051211

--- assets/js/common.js
+++ assets/js/common.js
@@ -54,7 +54,7 @@
     });
 
     $(document).click(function (e) {
-        var container = $("#mobile-nav, .mobile-nav-toggle");
+        var container = $("body .mobile-nav-toggle");
         if (!container.is(e.target) && container.has(e.target).length === 0) {
             if ($('body').hasClass('mobile-nav-active')) {
                 $('body').removeClass('mobile-nav-active');
